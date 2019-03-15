d8fd0b7c3c7fc3cd9527ff2cadb438fa49b36631
Morten Ledum <mortele@student.matnat.uio.no>
2019-03-15 17:50:47 +0100

    Flesh out 2d example with franke function

--- occam_bayesian_2d.py
+++ occam_bayesian_2d.py
@@ -3,3 +3,62 @@ import matplotlib.pyplot as plt
 from bayes_opt import BayesianOptimization
 from visualize_progress import plot_gp
 from franke import franke
+
+param_file_name = 'param.dat'
+result_file_name = 'res.dat'
+
+
+def change_parameters(*params):
+    """Sets the parameters for the current simulation run"""
+    with open(param_file_name, 'w') as out_file:
+        for p in params:
+            out_file.write(str(p) + '\n')
+
+
+def run_simulation():
+    """Start the actual simulation run"""
+    params = []
+    with open(param_file_name, 'r') as in_file:
+        for line in in_file:
+            params.append(float(line))
+
+    with open(result_file_name, 'w') as out_file:
+        f = franke(*tuple(params))
+        out_file.write(str(f))
+
+
+def extract_results():
+    """Extracts the results from the last simulation run"""
+    res = []
+    with open(result_file_name, 'r') as in_file:
+        for line in in_file:
+            res.append(float(line))
+    return res
+
+
+def cost(result, target):
+    """Cost function to maximize"""
+    return - (result - target)**2
+
+
+def optimize_2d(path=None, steps=None, init_points=None, bounds=None):
+    target = 1.5
+
+    def wrapper(x, y):
+        change_parameters(x, y)
+        run_simulation()
+        res = extract_results()
+        return cost(res[0], target)
+
+    opt = BayesianOptimization(f=wrapper,
+                               pbounds=bounds,
+                               verbose=2,
+                               random_state=92898)
+    opt.maximize(init_points=init_points, n_iter=0)
+    opt.maximize(init_points=0, n_iter=steps)
+    print(opt.max)
+
+
+if __name__ == '__main__':
+    optimize_2d(steps=10, init_points=10,
+                bounds={'x': (0, 1), 'y': (0, 1)})
