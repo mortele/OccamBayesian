a7ca1a60dc948d0031f9f4194e95f474b5cf526d
Morten Ledum <mortele@student.matnat.uio.no>
2019-04-01 14:36:17 +0200

    Stash

--- skopt_1d_test.py
+++ skopt_1d_test.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 
 
 def target(x, noise_variance=0.0):
-    return (np.sin(5 * x) * (1 - np.tanh(x**2))
-            + np.random.randn(*x.shape) * noise_variance)
+    y = (np.sin(5 * x) * (1 - np.tanh(x**2)
+         + np.random.randn(*np.array(x).shape) * noise_variance))
+    return y
 
 
-def main():
+def setup_example():
     noise_variance = 0.1
     x = np.linspace(-2, 2, 400)
     f_exact = target(x, noise_variance=0.0)
+    plt.figure(1)
     plt.plot(x, f_exact, 'k-', label='true target')
     plt.fill_between(x,
                      f_exact - 1.96 * noise_variance,
@@ -23,11 +25,43 @@ def main():
 
     skip = 5
     f_sampled = target(x[::skip], noise_variance=noise_variance)
-    plt.plot(x[::skip], f_sampled, 'b^', label='sampled')
+    plt.plot(x[::skip], f_sampled, 'b^', label='random sampled')
     plt.xlim(-2, 2)
     plt.grid()
-    plt.show()
+    plt.pause(1e-5)
+    return x, f_exact, noise_variance
+
+
+def opt_example():
+    x, f_exact, noise_variance = setup_example()
+    plt.figure(2)
+    plt.plot(x, f_exact, 'k-', label='true target')
+    plt.fill_between(x,
+                     f_exact - 1.96 * noise_variance,
+                     f_exact + 1.96 * noise_variance,
+                     color='r', alpha=0.3)
+    plt.xlabel('x')
+    plt.ylabel('target')
+
+    opt = skopt.Optimizer([(-2, 2)], 'GP', acq_func='EI',
+                          n_initial_points=0)
+    for _ in range(5):
+        x_next = np.random.uniform(-2, 2)
+        opt.tell([x_next], target(x_next, noise_variance=noise_variance))
+
+    for i in range(10):
+        for j in range(5):
+            x_next = opt.ask()
+            opt.tell(x_next, target(x_next[0], noise_variance=noise_variance))
+
+        # mu, s = opt.models[-1].predict(x.reshape(-1, 1), return_std=True)
+        plt.plot(opt.Xi, opt.yi, 'bv')
+        plt.pause(1e-5)
+    plt.legend()
+    plt.xlim(-2, 2)
+    plt.grid()
 
 
 if __name__ == '__main__':
-    main()
+    opt_example()
+    plt.show()
